87e470e5eb88b748678bc2b6ffde36219b904588
Jameson Welling <jamowelling@gmail.com>
2016-10-22 23:22:32 -0600

    refactor main.js, add code to retrieve a UID

--- webContent/js/main.js
+++ webContent/js/main.js
@@ -5,23 +5,3 @@ function initService() {
 }
 
 document.addEventListener( "DOMContentLoaded", initService);
-
-
-
-
-var tetra = require('./libs/tetra.js'); //includes the tetra library
-
-contactLessService = tetra.service({  //Instantiates service 
-  service: 'local.device.contactless0', 
-  namespace: 'ingenico.device.contactless'
-});
-
-function grabUID() {
-  
-  contactLessService
-    .reset()  //reset service
-    .call('GetUid', { requestDelay: 0}) //call the GetUid method
-    .success(function(r) {
-      console.log(r); //Handle a successful UID retrieval
-  })
-}
